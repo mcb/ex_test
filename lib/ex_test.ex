ff9a738cf649d011d54d11f4672b6eb1005ba44a
Michael C. Beck <michael.beck@qualeapps.com>
2016-02-17 20:50:31 +0100

    support setup within describe and added describe

--- lib/ex_test.ex
+++ lib/ex_test.ex
@@ -2,7 +2,7 @@ defmodule ExTest do
   defmacro __using__(_) do
     quote do
       use ExUnit.Case
-      import ExTest
+      import ExTest, except: [setup: 1, setup: 2]
     end
   end
 
@@ -15,6 +15,42 @@ defmodule ExTest do
   defmacro it(name, context, options) do
     quote do
       test("it #{unquote name}", unquote(context), unquote(options))
+    end
+  end
+
+  defmacro setup(context_name, [do: block]) do
+    quote do
+      ExUnit.Callbacks.setup(context) do
+        {:ok, unquote(context_name)} = @calling_module.__ex_unit__(:setup, context)
+        unquote(block)
+      end
+    end
+  end
+
+  defmacro setup([do: block]) do
+    quote do
+      raise "Calling setup without a context is unsupported"
+    end
+  end
+
+  defmacro describe(string, [do: block]) do
+    module = string
+          |> String.split(~r/\W/)
+          |> Enum.map(&String.capitalize/1)
+          |> Enum.join
+          |> List.wrap
+          |> Module.concat
+
+    calling_module = __CALLER__.module
+
+    quote do
+      defmodule unquote(module) do
+        use ExUnit.Case
+        import ExUnit.Callbacks, except: [setup: 1, setup: 2]
+        import ExTest
+        @calling_module unquote( calling_module )
+        unquote(block)
+      end
     end
   end
 end
--- test/ex_test_test.exs
+++ test/ex_test_test.exs
@@ -5,15 +5,29 @@ defmodule ExTestTest do
     {:ok, Dict.put( context, :outer, :setup) }
   end
 
-  test 'test still works as before', context do
+  test "test still works as before", context do
     assert context[:outer] == :setup
   end
 
-  it 'works as before just using "it"' do
+  it "'works as before just using 'it'" do
     assert 0 == 0
   end
 
-  it 'works as before just using "it" with context', context do
+  it "works as before just using 'it' with context", context do
     assert context[:outer] == :setup
+  end
+
+  describe "with surrounding describe" do
+    setup context do
+      {:ok, Dict.put( context, :inner, :setup) }
+    end
+
+    it "still asserts correctly without context" do
+      assert 0 == 0
+    end
+
+    it "still asserts correctly with context", context do
+      assert context[:inner] == :setup
+    end
   end
 end
