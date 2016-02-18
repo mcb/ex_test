d377b4bc95b34f84060e5438d597b26c7ba4fc05
Michael C. Beck <michael.beck@qualeapps.com>
2016-02-18 19:49:37 +0100

    added hex metadata

--- mix.exs
+++ mix.exs
@@ -2,12 +2,19 @@ defmodule ExTest.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ex_test,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :ex_test,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      source_url: "https://github.com/mcb/ex_test",
+      description: "Wrapper around ExUnit to support BBD (rspec) like syntax",
+      package: package,
+      deps: deps,
+      docs: [
+        main: ExTest,
+        readme: "README.md"
+      ]
+    ]
   end
 
   # Configuration for the OTP application
@@ -17,16 +24,15 @@ defmodule ExTest.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp package do
+    [
+      maintainers: ["Michael C. Beck"],
+      licenses: ["MIT"],
+      links: %{"github" => "https://github.com/mcb/ex_test"}
+    ]
   end
 end
